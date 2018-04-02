60f6d90508965582013bcf082692424826df4138
admin <Mistermo716@gmail.com>
2018-04-02 08:55:38 -0700

    Array Double

--- Recursion/main.js
+++ Recursion/main.js
@@ -1,11 +1,27 @@
 'use strict';
+
+//Sheep Counter
 function sheepCounter(num) {
   if (num === 0) {
-    return '';
+    return;
   }
 
   console.log(`${num} - Another sheep jump over the fence`);
   sheepCounter(num - 1);
 }
 
-sheepCounter(3);
+//sheepCounter(3);
+
+//Array Double
+function arrDouble(arr) {
+  if (arr.length === 0) {
+    return [];
+  }
+
+  const newArr = arr[0] * 2;
+
+  return [newArr, ...arrDouble(arr.slice(1))];
+}
+
+// console.log(arrDouble([1, 2, 3, 4, 5]));
+// console.log(arrDouble([20, 40, 60, 80]));
